2dc2e90f1507425c06c94ece918143ec7257086a
Jack Lawson <ajacksified@gmail.com>
2014-06-10 11:27:00 -0700

    Initial.

---
+++ parent.lua
@@ -0,0 +1,32 @@
+#!/usr/bin/env lua
+
+function parent_string(level)
+  local ats = { '@' }
+  local parents = { }
+
+  while level > 0 do
+    table.insert(ats, '@')
+
+    if level == 1 then
+      table.insert(parents, 'parent')
+    elseif level == 2 then
+      table.insert(parents, 'grand')
+    elseif level > 2 then
+      table.insert(parents, 'great')
+    end
+
+    level = level - 1
+  end
+
+
+  return table.concat(ats, '') .. table.concat(parents, '')
+end
+
+-- if command-line usage:
+
+if arg and #arg > 0 then
+  local string = parent_string(tonumber(arg[1]))
+  print(string)
+end
+
+return parent_string
